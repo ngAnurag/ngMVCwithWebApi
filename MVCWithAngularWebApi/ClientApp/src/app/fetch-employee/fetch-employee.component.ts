09fdc58daa1bd6c28379fdbb6c4d7534d8dcf5b9
ngAnurag <ansh.pro@gmail.com>
2019-01-27 22:06:12 +0530

    Added new Components AddEmplolyee & FetchEmployee

---
+++ MVCWithAngularWebApi/ClientApp/src/app/fetch-employee/fetch-employee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fetch-employee',
+  templateUrl: './fetch-employee.component.html',
+  styleUrls: ['./fetch-employee.component.css']
+})
+export class FetchEmployeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
