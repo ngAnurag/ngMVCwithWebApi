dfa6cb1d6a28546a53d0f48f942ee8ee730ece18
ngAnurag <ansh.pro@gmail.com>
2019-01-28 08:47:50 +0530

    Added logic to fetch employee details

--- MVCWithAngularWebApi/ClientApp/src/app/fetch-employee/fetch-employee.component.ts
+++ MVCWithAngularWebApi/ClientApp/src/app/fetch-employee/fetch-employee.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Router, ActivatedRoute } from '@angular/router';
+import { EmployeeService } from '../Services/empservice.service'
 
 @Component({
   selector: 'app-fetch-employee',
   templateUrl: './fetch-employee.component.html',
   styleUrls: ['./fetch-employee.component.css']
 })
-export class FetchEmployeeComponent implements OnInit {
+export class FetchEmployeeComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  public empList: EmployeeData[];
+  constructor(public http: Http, private _router: Router, private _employeeService: EmployeeService) {
+    this.getEmployees();
   }
-
+  getEmployees() {
+    this._employeeService.getEmployees().subscribe(
+      data => this.empList = data
+    )
+  }
+  delete(employeeID) {
+    var ans = confirm("Do you want to delete customer with Id: " + employeeID);
+    if (ans) {
+      this._employeeService.deleteEmployee(employeeID).subscribe((data) => {
+        this.getEmployees();
+      }, error => console.error(error))
+    }
+  }
+}
+interface EmployeeData {
+  id: number;
+  name: string;
+  gender: string;
+  department: string;
+  city: string;
 }
